94eaa3237186786f0d5ab34e78ab7e802d1064b8
baredigit <git@baredigit.com>
2022-04-18 20:17:15 +0800

    add tableConfig in attributes

--- demoApp/index.js
+++ demoApp/index.js
@@ -1,7 +1,11 @@
 require('dotenv').config()
 const Bared = require('../src')
+// const UserPlugin = require('bared-user-plugin')
 
 async function run () {
+  // Bared.use(UserPlugin({
+  //   config: {}
+  // }))
   await Bared.start()
 }
 
--- src/api/user/user.schema.js
+++ src/api/user/user.schema.js
@@ -3,10 +3,16 @@ module.exports = {
   displayName: 'User',
   attributes: {
     name: { type: 'string', default: '', required: false },
-    avatar: { type: 'string', default: '', required: false },
+    avatar: {
+      type: 'string',
+      default: '',
+      required: false,
+      tableConfig: {
+        showAsAvatar: true
+      }
+    },
     username: { type: 'string', default: '', required: false, unique: true },
     password: { type: 'string', default: '', required: false },
-
     auth_type: {
       required: true,
       type: 'enum',
