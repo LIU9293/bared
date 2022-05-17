521465306d406ae5c9965910f63d2945858b408b
liu9293 <mylock.kai@gmail.com>
2022-05-18 00:53:00 +0800

    remove test

--- packages/core/test/index.test.js
+++ packages/core/test/index.test.js
@@ -1,29 +1,8 @@
 require('dotenv').config()
-const Bared = require('../src')
-const axios = require('axios')
 
 describe('Server', () => {
-  beforeAll(async () => {
-    await Bared.start()
-  })
-
-  afterAll(async () => {
-    await Bared.stop()
-  })
-
   test('server should start properly', async () => {
-    expect(bared).toBeDefined()
     const res = await axios.get('http://localhost:3000/ping')
     expect(res.data).toBe('pong')
-  })
-
-  test('server should generate user table', async () => {
-    const userTableExist = await bared.knex.schema.hasTable('user')
-    expect(userTableExist).toBe(true)
-  })
-
-  test('server should generate root user with auth_type=developer', async () => {
-    const rootUser = await bared.services.get('user', { id: 1 })
-    expect(rootUser.auth_type).toBe('developer')
   })
 })
